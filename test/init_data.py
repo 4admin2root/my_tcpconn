0f8f3b4bd5f1c2800847c5edc5123288eb031fea
4admin2root <piml.lui@gmail.com>
2017-05-15 13:30:09 +0800

    add init redis data

---
+++ test/init_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import  random
+import sys
+sys.path.append("../")
+from config import REDIS_HOST
+from config import REDIS_PORT
+from config import INTERVAL
+import redis
+
+
+i = 0
+conn = redis.Redis(host=REDIS_HOST,port=REDIS_PORT)
+pipe = conn.pipeline()
+
+if __name__ == '__main__':
+
+    while i < 1000:
+        ip = str(random.randint(192,255)) + '.' + \
+              str(random.randint(168,255)) + '.' + \
+              str(random.randint(0,255)) + '.' + \
+              str(random.randint(0,255)) + '_' + \
+              str(random.randint(1024,65535))
+        num = random.randint(1024,65535)
+        pipe.set(ip,num,INTERVAL)
+        i +=1
+    pipe.execute()
