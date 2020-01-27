87f16f319eb1fc093bada53731464737bbe8d9d2
lvzhijun <lvzhjc@yonyou.com>
2020-01-27 21:48:27 +0800

    add parameter KEEP_DATA to control keep tcp data or not

--- client.py
+++ client.py
@@ -17,9 +17,7 @@ links:
 
 import requests
 import json
-from config import SERVER_HOST
-from config import SERVER_PORT
-from config import INTERVAL
+import config
 from collections import Counter
 import logging
 import logging.config
@@ -33,29 +31,30 @@ __version__ = '0.0.1'
 __author__ = 'adminroot'
 __license__ = 'BSD 3-Clause License'
 
-#todo
+# todo
 # daemon and windows service
 #
 
 lports = []  # listen ports
-ltor = []    # local to remote links
-rtol = []    # remote to local links
+ltor = []  # local to remote links
+rtol = []  # remote to local links
 logging_config = json.load(open('logging.json', 'r'))
 logging.config.dictConfig(logging_config)
 logger = logging.getLogger('mylogger')
 
+
 def postdata(cr):
     """post links to http server"""
-    url = 'http://' + SERVER_HOST + ':' + str(SERVER_PORT) + '/tc/api/v1.0/tclist'
+    url = 'http://' + config.SERVER_HOST + ':' + str(config.SERVER_PORT) + '/tc/api/v1.0/tclist'
     headers = {'content-type': 'application/json'}
     for i in cr:
-        payload = {'tcp_conn_key': i, 'tcp_conn_value': cr[i], 'tcp_conn_interval': INTERVAL}
+        payload = {'tcp_conn_key': i, 'tcp_conn_value': cr[i], 'tcp_conn_interval': config.INTERVAL}
         try:
             requests.post(url, data=json.dumps(payload), headers=headers, timeout=3)  # todo :  bad perf
         except RequestException as re:
             logger.error("Post data failed: {0}".format(re))
-            logger.debug('SERVER_HOST:' + SERVER_HOST)
-            logger.debug('SERVER_PORT:' + str(SERVER_PORT))
+            logger.debug('SERVER_HOST:' + config.SERVER_HOST)
+            logger.debug('SERVER_PORT:' + str(config.SERVER_PORT))
             # exit when post failed
             # exit(1) 
 
@@ -70,10 +69,12 @@ def getlist():
     for i in tcs:
         if i.status == 'ESTABLISHED' and i.laddr.ip != '127.0.0.1':
             if i.laddr.port in lports:
-                rtol.append('tc' + '_' + i.raddr.ip.lstrip('::ffff:') + '_' + i.laddr.ip.lstrip('::ffff:') + '_' + str(i.laddr.port))
+                rtol.append('tc' + '_' + i.raddr.ip.lstrip('::ffff:') + '_' + i.laddr.ip.lstrip('::ffff:') + '_' + str(
+                    i.laddr.port))
             # elif i.raddr[0] != SERVER_HOST and i.raddr[1] != SERVER_PORT:
             else:
-                ltor.append('tc' + '_' + i.laddr.ip.lstrip('::ffff:') + '_' + i.raddr.ip.lstrip('::ffff:') + '_' + str(i.raddr.port))
+                ltor.append('tc' + '_' + i.laddr.ip.lstrip('::ffff:') + '_' + i.raddr.ip.lstrip('::ffff:') + '_' + str(
+                    i.raddr.port))
 
 
 def clearlist():
@@ -93,10 +94,11 @@ def run():
     logger.debug('start to post data to server')
     postdata(ltor_counter)
     postdata(rtol_counter)
-    clearlist()
+    if not config.KEEP_DATA:
+        clearlist()
 
 
 if __name__ == '__main__':
     while True:
         run()
-        time.sleep(INTERVAL)
+        time.sleep(config.INTERVAL)
